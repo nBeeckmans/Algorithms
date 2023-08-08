d13b176ad00cc258ada042599be1f024b0909251
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2023-08-08 11:51:08 -0400

    Update QuickSort.java

--- BadQuickSort/QuickSort.java
+++ BadQuickSort/QuickSort.java
@@ -5,11 +5,10 @@ public class QuickSort{
      * implementation of QuickSort on a list of Type T, T has to implement Comparable.
      */
     public static <T extends Comparable<T>> void quickSort(ArrayList<T> list, int firstIndex, int lastIndex ){
-        int nextMiddleIndex; 
-        if ( firstIndex < lastIndex && firstIndex >= 0 && lastIndex < list.size()) {
-            nextMiddleIndex = sorting(list, firstIndex, lastIndex); 
-            quickSort(list,0, nextMiddleIndex); 
-            quickSort(list, nextMiddleIndex + 1, list.size()-1); 
+        if ( firstIndex < lastIndex) {
+            int nextMiddleIndex = sorting(list, firstIndex, lastIndex); 
+            quickSort(list,firstIndex, nextMiddleIndex); 
+            quickSort(list, nextMiddleIndex + 1, lastIndex); 
         }
     }
     /**
@@ -17,29 +16,28 @@ public class QuickSort{
      */
     public static <T extends Comparable<T>> int sorting(ArrayList<T> list, int firstIndex,int lastIndex){
         T pivot = list.get( (firstIndex + lastIndex)/2 );
-        firstIndex --;
+        firstIndex--;
         lastIndex++;
         while( firstIndex < lastIndex ) {
             do {
                 firstIndex++; 
-            } while ( firstIndex < list.size() && list.get(firstIndex).compareTo(pivot) < 0 ); 
+            } while (list.get(firstIndex).compareTo(pivot) < 0 ); 
             do {
                 lastIndex--;
-            } while(lastIndex > -1 && list.get(lastIndex).compareTo(pivot) > 0);
+            } while(list.get(lastIndex).compareTo(pivot) > 0);
             if (firstIndex < lastIndex) {
                 swap(list, firstIndex, lastIndex);
             }
         } 
+        System.out.println(list);
         return lastIndex; 
     }
+    /**
+     * implementation of the swaping method. 
+     */
     public static <T> void swap(ArrayList<T> list, int firstIndex, int lastIndex) {
-        
-            T temp = list.remove(firstIndex); 
-            list.add(firstIndex, list.remove(lastIndex));
-            list.add(firstIndex, temp);
-
-            
+            T temp = list.get(firstIndex); 
+            list.set(firstIndex, list.get(lastIndex));
+            list.set(lastIndex, temp);       
     }
-
-
 }
