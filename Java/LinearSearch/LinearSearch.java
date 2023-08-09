2963d77c2e51c6fbca4389115e134c959c3d81fc
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2023-08-09 16:22:45 -0400

    adding LinearSearch and SelectionSort

--- Java/LinearSearch/LinearSearch.java
+++ Java/LinearSearch/LinearSearch.java
@@ -1,5 +1,14 @@
 package Java.LinearSearch;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
-    
+    public static <T> int search(ArrayList<T> list, T toFind){
+        boolean isNotFound = true;
+        int index = 0; 
+        while (isNotFound && index < list.size()){
+            isNotFound = list.get(index).equals(toFind);
+        }
+        return isNotFound ? -1 : index; 
+    }
 }
--- Java/SelectionSort/Selection.java
+++ Java/SelectionSort/Selection.java
@@ -1,5 +1,26 @@
 package Java.SelectionSort;
 
+import java.util.ArrayList;
+
 public class Selection {
-    
+    public static <T extends Comparable<T>> void sort(ArrayList<T> list) {
+        for (int i = 0; i < list.size() - 1; i++){
+            T minimum = list.get(i);
+            int j = i + 1;
+            for (; j < list.size(); j++){
+                minimum = list.get(j).compareTo(minimum) < 0 
+                ? list.get(j) 
+                : minimum; 
+            }
+            if (list.get(i).compareTo(minimum) > 0){
+                swap(list, i, j); 
+            }
+        }
+    }
+
+    private static <T> void swap (ArrayList<T> list,int i,int j) {
+        T temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    } 
 }
