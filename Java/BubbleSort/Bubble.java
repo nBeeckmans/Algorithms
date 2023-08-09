44e238f6f6c872bd8ac49fd9b8ae8bb8e40b0d0c
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2023-08-09 16:36:08 -0400

    adding bubbles !

--- Java/BubbleSort/Bubble.java
+++ Java/BubbleSort/Bubble.java
@@ -1,5 +1,26 @@
 package Java.BubbleSort;
 
+import java.util.ArrayList;
+
 public class Bubble {
-    
+    public static <T extends Comparable<T>> void sort(ArrayList<T> list) {
+        boolean isNotSorted = true;  
+        int lastIndex = list.size() - 2; 
+        while (isNotSorted) {
+            isNotSorted = false; 
+            for (int i = 0; i < lastIndex; i ++) {
+                if (list.get(i).compareTo( list.get(i+1)) > 0) {
+                    swap(list, i, i + 1);
+                    isNotSorted = true; 
+                }
+            }
+            lastIndex --; 
+        }
+    } 
+
+    private static <T> void swap (ArrayList<T> list,int i,int j) {
+        T temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    } 
 }
